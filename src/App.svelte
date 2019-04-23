4ed2b5da50b12fbdfeae35c76e96c59065738692
Gabe M <gabemeola@gmail.com>
2019-04-22 23:24:25 -0600

    add card component

--- src/App.svelte
+++ src/App.svelte
@@ -1,3 +1,10 @@
 <div>
   Hello World
+  <Card name="Gabe">
+    hello
+  </Card>
 </div>
+
+<script>
+  import Card from './components/Card';
+</script>
---
+++ src/components/Card.svelte
@@ -0,0 +1,15 @@
+<div class="card">
+  <slot></slot>
+</div>
+
+<style>
+  .card {
+    height: 400px;
+    width: 200px;
+    border: 1px solid gray;
+  }
+</style>
+
+<script>
+  export let name;
+</script>
